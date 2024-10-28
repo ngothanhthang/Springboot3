b81316cb28c9aad1602e872c814bee4304607653
ngothanhthang <ngothanhthang2309@gmail.com>
2024-10-28 10:28:03 +0700

    project

---
+++ src/main/java/vn/iostar/CategorySpringboot3Application.java
@@ -0,0 +1,13 @@
+package vn.iostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorySpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorySpringboot3Application.class, args);
+	}
+
+}
